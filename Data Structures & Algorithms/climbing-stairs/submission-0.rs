f12c619431db2bd452e33c727375202354691f21
Will Urban <will.urban.dev@gmail.com>
2026-07-18 16:24:12 +0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let mut one = 1;
+        let mut two = 1;
+
+        for _ in 0..(n-1) {
+            let temp = one;
+            one = two + one;
+            two = temp;
+        }
+
+        one
+    }
+}
