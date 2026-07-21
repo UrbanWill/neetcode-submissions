83d243c34cd589c4bca221ed1643b7c5e576b1d4
Will Urban <will.urban.dev@gmail.com>
2026-07-21 10:02:42 +0700

    Add: number-of-students-unable-to-eat-lunch - submission-6

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-6.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn count_students(students: Vec<i32>, sandwiches: Vec<i32>) -> i32 {
+        let mut res = students.len() as i32;
+
+        let mut ctn = students.iter().fold(HashMap::new(), |mut map, stdn| {
+            *map.entry(stdn).or_insert(0) += 1;
+            map
+        });
+
+        for s in sandwiches.iter() {
+            match ctn.get_mut(s) {
+                Some(c) if *c > 0 => {
+                    *c -= 1;
+                    res -= 1;
+                },
+                _ => {
+                    return res;
+                }
+            }
+        };
+
+        res
+    }
+}
