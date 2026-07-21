d2d5af7abfa783b61780a73153b9209ab895baf1
Will Urban <will.urban.dev@gmail.com>
2026-07-21 10:17:28 +0700

    Add: mergeSort - submission-6

---
+++ Data Structures & Algorithms/mergeSort/submission-6.ts
@@ -0,0 +1,68 @@
+/** Pair class to store key-value pairs */
+// class Pair {
+//   /**
+//    * @param {number} key The key to be stored in the pair
+//    * @param {string} value The value to be stored in the pair
+//    */
+//   constructor(key, value) {
+//       this.key = key;
+//       this.value = value;
+//   }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[]}
+     */
+    mergeSort(pairs: Pair[]): Pair[] {
+        return this.mergeSortHelper(pairs, 0, pairs.length - 1);
+    };
+
+    mergeSortHelper(pairs, s, e) {
+        if (e - s + 1 <= 1) {
+            return pairs;
+        }
+
+        const m = Math.floor((e + s) / 2);
+
+        this.mergeSortHelper(pairs, s, m);
+
+        this.mergeSortHelper(pairs, m + 1, e);
+
+        this.merge(pairs, s, m, e);
+
+        return pairs;
+    };
+
+    merge(arr, s, m, e) {
+        const l = arr.slice(s, m + 1);
+        const r = arr.slice(m + 1, e + 1);
+
+        let i = 0;
+        let j = 0;
+        let k = s;
+
+        while (i < l.length && j < r.length) {
+            if (l[i].key <= r[j].key) {
+                arr[k] = l[i];
+                i += 1;
+            } else {
+                arr[k] = r[j]; 
+                j += 1;
+            }
+            k +=1;
+        }
+
+        while (i < l.length) {
+            arr[k] = l[i];
+            i += 1;
+            k += 1;
+        }
+
+        while (j < r.length) {
+            arr[k] = r[j];
+            j += 1;
+            k += 1;
+        }
+    };
+}
