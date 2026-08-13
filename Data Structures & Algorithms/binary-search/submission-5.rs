56ca57765e4b0b4b8d8b30b287d61d3c2648cb65
Will Urban <will.urban.dev@gmail.com>
2026-08-13 15:27:29 +0700

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut l = 0;
+        let mut r = nums.len() as i32 - 1;
+
+        while l <= r {
+            let m = (l + (r - l) / 2);
+            let cur = nums[m as usize];
+
+            if target > cur {
+                l = m + 1;
+            } else if target < cur {
+                r = m - 1;
+            } else {
+                return m as i32;
+            }
+        }
+
+        -1
+    }
+}
