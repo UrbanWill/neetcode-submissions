cbf5d4ecc1a1a8780a6a3ffb578e2ff3ea09bd82
Will Urban <will.urban.dev@gmail.com>
2026-08-21 14:40:43 +0700

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn combination_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut res = Vec::new();
+        let mut subset = Vec::new();
+        Self::dfs(&nums, 0, 0, target, &mut subset, &mut res);
+        res
+    }
+
+    fn dfs(nums: &Vec<i32>, i: usize, total: i32, target: i32, subset: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
+        if i >= nums.len() || total > target {
+            return;
+        }
+
+        if total == target {
+            res.push(subset.clone());
+            return;
+        }
+
+        subset.push(nums[i]);
+        Self::dfs(nums, i, total + nums[i], target, subset, res);
+        
+        subset.pop();
+        Self::dfs(nums, i + 1, total, target, subset, res);
+    }
+}
