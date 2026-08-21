70d2c2035428a080a70f9ffcdf52e2f2594b664f
Will Urban <will.urban.dev@gmail.com>
2026-08-21 09:15:40 +0700

    Add: kth-smallest-integer-in-bst - submission-5

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-5.rs
@@ -0,0 +1,37 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//     pub val: i32,
+//     pub left: Option<Rc<RefCell<TreeNode>>>,
+//     pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         TreeNode {
+//             val,
+//             left: None,
+//             right: None,
+//         }
+//     }
+// }
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        let mut node_vals = Vec::new();
+        Self::dfs(&root, &mut node_vals);
+        node_vals[k as usize -1]
+    }
+    fn dfs(root: &Option<Rc<RefCell<TreeNode>>>, node_vals: &mut Vec<i32>) {
+        if let Some(node) = root {
+            let n = node.borrow();
+            Self::dfs(&n.left, node_vals);
+            node_vals.push(n.val);
+            Self::dfs(&n.right, node_vals);
+        }
+    }
+}
