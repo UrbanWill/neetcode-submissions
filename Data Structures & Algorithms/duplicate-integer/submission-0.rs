82f9f86e11984b3112ef9d98c21009f1ea2dca68
Will Urban <will.urban.dev@gmail.com>
2026-08-22 18:54:50 +0700

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.rs
@@ -0,0 +1,9 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let set: HashSet<i32> = nums.iter().copied().collect();
+
+        set.len() < nums.len()
+    }
+}
