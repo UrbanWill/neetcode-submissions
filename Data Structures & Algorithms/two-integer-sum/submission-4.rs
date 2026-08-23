3221d9b35a0d04cc8ba2812c94bb2bb98eb1847f
Will Urban <will.urban.dev@gmail.com>
2026-08-23 14:15:44 +0700

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+
+        for i in 0..nums.len() {
+            let diff = target - nums[i];
+
+            if let Some(diff_index) = map.get(&diff) {
+                return vec![*diff_index as i32, i as i32];
+            }
+
+            map.insert(nums[i], i);
+        }
+
+        vec![]
+    }
+}
