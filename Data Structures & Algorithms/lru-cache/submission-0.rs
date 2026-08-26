f89131d177d9f7fd1cf1ed1d177bf52033fe5121
Will Urban <will.urban.dev@gmail.com>
2026-08-26 15:09:47 +0700

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.rs
@@ -0,0 +1,163 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::{Rc, Weak};
+
+type NodePtr = Rc<RefCell<Node>>;
+type WeakNodePtr = Weak<RefCell<Node>>;
+
+#[derive(Debug)]
+struct Node {
+    key: i32,
+    value: i32,
+    prev: Option<WeakNodePtr>,
+    next: Option<NodePtr>
+}
+
+impl Node {
+    fn new(key:i32, value: i32) -> Self {
+        Self {
+            key,
+            value,
+            prev: None,
+            next: None
+        }
+    }
+}
+
+struct LRUCache {
+    capacity: usize,
+    map: HashMap<i32, NodePtr>,
+    head: Option<NodePtr>,
+    tail: Option<NodePtr>
+}
+
+impl LRUCache {
+    pub fn new(capacity: i32) -> Self {
+        Self {
+            capacity: capacity.max(0) as usize,
+            // map: HashMap::with_capacity(capacity as usize),
+            map: HashMap::new(),
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn get(&mut self, key: i32) -> i32 {
+        let Some(node) = self.map.get(&key).cloned() else {
+            return - 1;
+        };
+
+        let value = node.borrow().value;
+
+        self.remove(&node);
+        self.push_front(&node);
+
+        value
+    }
+
+    pub fn put(&mut self, key: i32, value: i32) {
+        if self.capacity == 0 {
+            return;
+        }
+
+        // Existing key: update it and mark it MRU
+        if let Some(node) = self.map.get(&key).cloned() {
+            node.borrow_mut().value = value;
+
+            self.remove(&node);
+            self.push_front(&node);
+
+            return;
+        }
+
+        // New key
+
+        let node = Rc::new(RefCell::new(Node::new(key, value)));
+
+        self.push_front(&node);
+        self.map.insert(key, node);
+
+        // Evit the least recenty used node
+
+        if self.map.len() > self.capacity {
+            if let Some(lru) = self.pop_back() {
+                let key = lru.borrow().key;
+                self.map.remove(&key);
+            }
+        }
+    }
+
+    fn push_front(&mut self, node: &NodePtr) {
+        let old_head = self.head.take();
+
+
+        // Update the new front node
+        {
+            let mut node_ref = node.borrow_mut();
+            node_ref.prev = None;
+            node_ref.next = old_head.clone();
+        }
+
+        // Update the old head
+        match old_head {
+            Some(head) => {
+                head.borrow_mut().prev = Some(Rc::downgrade(node));
+            }
+            None => {
+                // Empty list: this node is also the tail.
+                self.tail = Some(Rc::clone(node));
+            }
+        }
+
+
+        // Update head
+        self.head = Some(Rc::clone(node))
+    }
+
+    fn remove(&mut self, node: &NodePtr) {
+        // Completely detach the node first
+
+        let (prev, next) = {
+            let mut node_ref = node.borrow_mut();
+
+            let prev = node_ref.prev.take();
+            let next = node_ref.next.take();
+
+            (prev, next)
+        };
+
+        let prev_node = prev.as_ref().and_then(Weak::upgrade);
+        
+
+        // Connect previous node to next node.
+        match &prev_node {
+            Some(prev_node) => {
+                prev_node.borrow_mut().next = next.clone();
+            },
+            None => {
+                // No previous node means node was the head.
+                self.head = next.clone();
+            }
+        }
+
+        // Connect next node to previous node.
+        match &next {
+            Some(next_node) => {
+                next_node.borrow_mut().prev = prev_node.as_ref().map(Rc::downgrade);
+            },
+            None => {
+                // No next node means node was the tail.
+                self.tail = prev_node;
+            }
+        }
+
+    }
+
+    fn pop_back(&mut self) -> Option<NodePtr> {
+        let tail = self.tail.clone()?;
+
+        self.remove(&tail);
+
+        Some(tail)
+    }
+}
