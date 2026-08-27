c90cb4305dd837ead6452271f724e3dafc70be7e
Will Urban <will.urban.dev@gmail.com>
2026-08-27 16:01:49 +0700

    Add: path-sum - submission-7

---
+++ Data Structures & Algorithms/path-sum/submission-7.rs
@@ -0,0 +1,42 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//     pub val: i32,
+//     pub left: Option<Rc<RefCell<TreeNode>>>,
+//     pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         TreeNode {
+//             val,
+//             left: None,
+//             right: None,
+//         }
+//     }
+// }
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
+        Self::dfs(&root, target_sum, 0)
+    }
+
+    fn dfs(root: &Option<Rc<RefCell<TreeNode>>>, target: i32, sum: i32) -> bool {
+        match root {
+            None => false,
+            Some(node) => {
+                let n = node.borrow();
+                let new_sum = n.val + sum;
+
+                match (&n.left, &n.right) {
+                    (None, None) => new_sum == target,
+                    _ => Self::dfs(&n.left, target, new_sum) || Self::dfs(&n.right, target, new_sum)
+                }
+            }
+        }
+    }
+}
