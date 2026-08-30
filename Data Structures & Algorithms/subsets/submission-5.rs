21712032be94f8bfaa7a71e923b4088dc7a58870
Will Urban <will.urban.dev@gmail.com>
2026-08-30 09:21:42 +0700

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut sub = Vec::new();
+        let mut res = Vec::new();
+        Self::dfs(&nums, &mut sub, &mut res, 0);
+
+        res
+    }
+
+    fn dfs(nums: &Vec<i32>, sub: &mut Vec<i32>, res: &mut Vec<Vec<i32>>, i: usize) {
+        if i >= nums.len() {
+            res.push(sub.clone());
+            
+            return;
+        }
+
+        sub.push(nums[i]);
+        Self::dfs(nums, sub, res, i + 1);
+
+        sub.pop();
+        Self::dfs(nums, sub, res, i + 1);
+    }
+}
