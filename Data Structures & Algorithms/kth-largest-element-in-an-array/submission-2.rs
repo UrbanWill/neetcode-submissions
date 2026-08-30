0d639cd8ad58b7c845a5c1b4994a976b942efd48
Will Urban <will.urban.dev@gmail.com>
2026-08-30 09:06:21 +0700

    Add: kth-largest-element-in-an-array - submission-2

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-2.rs
@@ -0,0 +1,18 @@
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+
+impl Solution {
+    pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+        let mut min_heap = BinaryHeap::new();
+
+        for num in nums {
+            min_heap.push(Reverse(num));
+        }
+
+        while min_heap.len() > k as usize {
+            min_heap.pop();
+        }
+
+        min_heap.peek().unwrap().0
+    }
+}
