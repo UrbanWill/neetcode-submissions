18f3da67aa8a712c9cc9a3defa542aed97f6c75c
Will Urban <will.urban.dev@gmail.com>
2026-09-13 08:42:29 +0700

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
+        let mut count = 0;
+        let dir = vec![(0,1), (0, -1), (1,0), (-1,0)];
+        let (rows, cols) = (grid.len() as i32, grid[0].len() as i32);
+
+        for row in 0..rows {
+            for col in 0..cols {
+                if grid[row as usize][col as usize] == '1' {
+                    count += 1;
+                    Self::bfs(&mut grid, dir.clone(), row, col);
+                }
+            }
+        }
+
+        count
+    }
+
+    fn bfs(grid: &mut Vec<Vec<char>>, dir: Vec<(i32, i32)>, row: i32, col: i32) {
+        if row < 0 || col < 0 || row >= grid.len() as i32 || col >= grid[0].len() as i32 || grid[row as usize][col as usize] != '1' {
+            return;
+        }
+
+        grid[row as usize][col as usize] = '0';
+
+        for (nr, nc) in dir.iter() {
+            let (row, col) = (row + nr, col + nc);
+            Self::bfs(grid, dir.clone(), row, col);
+        }
+    }
+}
