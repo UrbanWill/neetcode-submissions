4441bf26c00e008da641b6ac92a7ffa3b0b281a8
Will Urban <will.urban.dev@gmail.com>
2026-09-13 14:26:04 +0700

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.rs
@@ -0,0 +1,51 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+/*
+// Definition for a Node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct Node {
+    pub val: i32,
+    pub neighbors: Vec<Rc<RefCell<Node>>>,
+}
+
+impl Node {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        Node {
+            val,
+            neighbors: Vec::new(),
+        }
+    }
+}
+*/
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn clone_graph(node: Option<Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
+        let mut cloned_map = HashMap::new();
+
+        Self::dfs(node, &mut cloned_map)
+    }
+
+    fn dfs(node: Option<Rc<RefCell<Node>>>, cloned_map: &mut HashMap<i32, Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
+        let node = node?;
+        let val = node.borrow().val;
+
+        match cloned_map.get(&val) {
+            Some(cloned) => Some(cloned.clone()),
+            None => {
+                let copy = Rc::new(RefCell::new(Node::new(val)));
+                cloned_map.insert(val, copy.clone());
+
+                for n in node.borrow().neighbors.iter() {
+                    if let Some(cloned) = Self::dfs(Some(n.clone()), cloned_map) {
+                        copy.borrow_mut().neighbors.push(cloned.clone());
+                    }
+                }
+                Some(copy)
+            }
+        }
+    }
+}
